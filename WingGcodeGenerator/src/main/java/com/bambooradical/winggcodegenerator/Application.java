17ebf46819742e56f99fde8527ef99a6978e53af
Peter Withers <peter-gthb@bambooradical.com>
2017-10-11 20:33:16 +0200

    Added appengine configuration and beans.

--- WingGcodeGenerator/src/main/java/com/bambooradical/winggcodegenerator/Application.java
+++ WingGcodeGenerator/src/main/java/com/bambooradical/winggcodegenerator/Application.java
@@ -3,13 +3,18 @@
  */
 package com.bambooradical.winggcodegenerator;
 
-import org.springframework.boot.SpringApplication;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.WebApplicationContext;
+
 
 /**
  * @since Aug 17, 2015 8:45:14 PM (creation date)
@@ -19,6 +24,7 @@ import org.springframework.web.context.WebApplicationContext;
 @ComponentScan
 @EnableAutoConfiguration
 //@EnableEntityLinks
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
@@ -35,5 +41,10 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected WebApplicationContext run(SpringApplication application) {
         return super.run(application);
+    }
+
+    @Bean
+    public Datastore cloudDatastoreService() {
+        return DatastoreOptions.defaultInstance().service();
     }
 }
