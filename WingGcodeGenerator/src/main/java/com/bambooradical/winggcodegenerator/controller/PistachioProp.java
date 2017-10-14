dc3c671ed364e6fee4e634a98e00279aa1f6c644
Peter Withers <peter-gthb@bambooradical.com>
2017-10-14 12:05:04 +0200

    Support for cloud datastore over crud.

--- WingGcodeGenerator/src/main/java/com/bambooradical/winggcodegenerator/controller/PistachioProp.java
+++ WingGcodeGenerator/src/main/java/com/bambooradical/winggcodegenerator/controller/PistachioProp.java
@@ -3,14 +3,13 @@
  */
 package com.bambooradical.winggcodegenerator.controller;
 
-import com.bambooradical.winggcodegenerator.dao.AccessDataRepository;
+import com.bambooradical.winggcodegenerator.dao.AccessDataService;
 import com.bambooradical.winggcodegenerator.model.AccessData;
 import com.bambooradical.winggcodegenerator.model.PistachioPropData;
 import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PistachioProp {
 
     @Autowired
-    AccessDataRepository accessDataRepository;
+    AccessDataService accessDataRepository;
 
     @RequestMapping("/PistachioProp")
     public String designView(
