07a2251d4b151f8aa7f36870efef069050ef9eb8
Peter Withers <peter-gthb@bambooradical.com>
2017-12-20 23:45:39 +0100

    Updated the defaults.

--- WingGcodeGenerator/src/main/java/com/bambooradical/winggcodegenerator/model/LaserTestGcodeData.java
+++ WingGcodeGenerator/src/main/java/com/bambooradical/winggcodegenerator/model/LaserTestGcodeData.java
@@ -13,8 +13,8 @@ public class LaserTestGcodeData {
 
     private int gridSize = 50;
     private int lineSpacing = 5;
-    private double lineSteps = 1;
-    private double lineZigzag = 5;
+    private double lineSteps = 5;
+    private double lineZigzag = 0.2;
     private int flySpeed = 3500;
     private int minPower = 100;
     private int maxPower = 300;
