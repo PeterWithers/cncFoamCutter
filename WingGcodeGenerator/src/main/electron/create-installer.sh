cad5d09fe085d49f10eb64528644012f408b4273
Peter Withers <peter.withers@mpi.nl>
2018-01-17 20:12:08 +0100

    Started a installer build script.

---
+++ WingGcodeGenerator/src/main/electron/create-installer.sh
@@ -0,0 +1,13 @@
+#npm install npm@4 -g
+
+#npm install -g electron-forge
+#electron-forge init winggcode
+cd winggcode
+#npm install data-table
+#npm install serialport
+#npm install serialport-v4
+#npm start
+#electron-forge package 
+#electron-forge make --platform=darwin --platform=win32
+#electron-forge make --platform=win32 --arch=x64 --target=zip
+electron-forge package --platform=win32 --arch=x64
