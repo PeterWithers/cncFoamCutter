9778be0bd568c38e7ef8af1becf7993e7f8382c9
Peter Withers <peter.withers@mpi.nl>
2018-01-17 20:05:52 +0100

    Updated the gcode control interface.

--- WingGcodeGenerator/src/main/electron/WingGcode.app/Contents/Resources/app/serialconnector.js
+++ WingGcodeGenerator/src/main/electron/WingGcode.app/Contents/Resources/app/serialconnector.js
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2018 Peter Withers
-*/
+ */
 
 /*
  * @since Jan 15, 2018 21:22 PM (creation date)
  * @author <peter-gthb@bambooradical.com>
-*/
+ */
 
 const serialport = require('serialport')
 
@@ -28,10 +28,43 @@ serialport.list((err, ports) => {
         portSelect.add(option);
     });
     document.getElementById("sendButton").onclick = sendGcode;
-})
+    document.getElementById("cancelButton").onclick = function () {
+        cancelRequest = true;
+    };
+    document.getElementById("leftButton").onclick = function () {
+        jogRequest(-10, 0);
+    };
+    document.getElementById("rightButton").onclick = function () {
+        jogRequest(10, 0);
+    };
+    document.getElementById("upButton").onclick = function () {
+        jogRequest(0, -10);
+    };
+    document.getElementById("downButton").onclick = function () {
+        jogRequest(0, 10);
+    };
+    document.getElementById("homeButton").onclick = function () {
+        document.getElementById("gcodeArea").value = "M05 S0 G90 G0 X0 Y0 Z0";
+        sendGcode();
+    };
+    document.getElementById("zeroButton").onclick = function () {
+        document.getElementById("gcodeArea").value = "M05 S0 G92 X0 Y0 Z0";
+        sendGcode();
+    };
+});
 
+function jogRequest(xDist, yDist) {
+    document.getElementById("gcodeArea").value =
+            "M05 S0\n" // turn off laser
+            + "G91\n" // Set to incremental Positioning
+            + "G21\n" // Millimeters
+            + "G0 X" + xDist + " Y" + yDist + "\n" // linear move
+    sendGcode();
+}
 var sendInProgress = false;
+var cancelRequest = false;
 function sendGcode() {
+    cancelRequest = false;
     if (!sendInProgress) {
         sendInProgress = true;
         var portSelect = document.getElementById("portlist");
@@ -40,25 +73,33 @@ function sendGcode() {
             if (err) {
                 document.getElementById("porterror").textContent = err.message;
                 sendInProgress = false;
+                cancelRequest = false;
                 return;
             }
         });
         var gcodeLines = document.getElementById("gcodeArea").value.split("\n");
         var gcodeTimerCallback = function () {
-            var lineToSend = gcodeLines.shift();
-            if (gcodeLines.length > 0) {
-                port.write(lineToSend + "\n", function (err) {
-                    if (err) {
-                        document.getElementById('porterror').textContent = err.message;
-                        sendInProgress = false;
-                        return;
-                    }
-                    document.getElementById('porterror').textContent = "";
-                    document.getElementById("gcodeArea").value = gcodeLines.join("\n");
-                    setTimeout(gcodeTimerCallback, 100);
-                });
-            } else {
+            if (cancelRequest) {
                 sendInProgress = false;
+                cancelRequest = false;
+            } else {
+                var lineToSend = gcodeLines.shift();
+                if (gcodeLines.length > 0) {
+                    port.write(lineToSend + "\n", function (err) {
+                        if (err) {
+                            document.getElementById('porterror').textContent = err.message;
+                            sendInProgress = false;
+                            cancelRequest = false;
+                            return;
+                        }
+                        document.getElementById('porterror').textContent = "";
+                        document.getElementById("gcodeArea").value = gcodeLines.join("\n");
+                        setTimeout(gcodeTimerCallback, 100);
+                    });
+                } else {
+                    sendInProgress = false;
+                    cancelRequest = false;
+                }
             }
         }
         setTimeout(gcodeTimerCallback, 100);
